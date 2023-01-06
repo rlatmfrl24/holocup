89ce27e66d383fc0afa9b957edc6f7600e8c5242
SulfurBottom <397love@gmail.com>
2023-01-06 16:43:35 +0900

    update

--- pages/prediction/result.tsx
+++ pages/prediction/result.tsx
@@ -1,15 +1,28 @@
 import { useRecoilValue } from "recoil";
 import Layout from "../../components/layout";
-import { group_a, group_b, group_c, newPredictionState } from "../../lib/store";
+import { apiEndpoint, newPredictionState } from "../../lib/store";
 import Image from "next/image";
-import { NextPage } from "next";
-import { getDummyResult, sortByDuplicates } from "../../lib/util";
-import { useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { sortByDuplicates } from "../../lib/util";
+import { useEffect, useState } from "react";
 import { PredictionData } from "../../lib/typeDef";
+import { useRouter } from "next/router";
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  return { props: {} };
+};
 
 const PredictionResult = () => {
   const predictionData = useRecoilValue(newPredictionState);
   const [totalScore, setTotalScore] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (predictionData.nickname === "") {
+      alert("새로고침 후에는 다시 로그인해야합니다ㅠ");
+      router.push("/login");
+    }
+  }, []);
 
   //   const [totalScore, setTotalScore] = useState(
   //     calulateScore(predictionData, resultData)
@@ -124,8 +137,8 @@ const PredictionResult = () => {
               <div key={team} className="flex-1">
                 <Image
                   src={`/images/entry/${team}.png`}
-                  width={100}
-                  height={100}
+                  width={200}
+                  height={200}
                   alt="winner"
                   style={{ objectFit: "contain" }}
                   className="flex-1"
@@ -142,7 +155,7 @@ const PredictionResult = () => {
               result === undefined
                 ? [...Array(12)].map((i) => (
                     <div key={i} className="flex-1">
-                      <div className="bg-gray-400 h-20 flex-1 flex items-center justify-center text-4xl font-bold rounded">
+                      <div className="bg-gray-400 h-20 flex items-center justify-center text-4xl font-bold rounded">
                         ?
                       </div>
                     </div>
@@ -151,8 +164,8 @@ const PredictionResult = () => {
                     <div key={team} className="flex-1">
                       <Image
                         src={`/images/entry/${team}.png`}
-                        width={100}
-                        height={100}
+                        width={200}
+                        height={200}
                         alt="winner"
                         style={{ objectFit: "contain" }}
                         className="flex-1"
